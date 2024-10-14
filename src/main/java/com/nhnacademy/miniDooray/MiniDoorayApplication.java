d674a628a5fa65935f9b6c3c40cbca1218be1722
wjdtjdgns <person3275@gmail.com>
2024-10-14 17:07:39 +0900

    cloud

--- src/main/java/com/nhnacademy/miniDooray/MiniDoorayApplication.java
+++ src/main/java/com/nhnacademy/miniDooray/MiniDoorayApplication.java
@@ -2,8 +2,10 @@ package com.nhnacademy.miniDooray;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MiniDoorayApplication {
 
 	public static void main(String[] args) {
